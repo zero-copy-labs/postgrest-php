b9ccfde527906aaba65603ec8023023d161955d7
WichinRighetti <lamrighetti@gmail.com>
2023-04-27 12:16:38 -0700

    removed extra variables (already in headers)

--- examples/call-a-postgres-fucntion/call-a-postgres-function-without-arguments.php
+++ examples/call-a-postgres-fucntion/call-a-postgres-function-without-arguments.php
@@ -3,12 +3,8 @@
 require '../header.php';
 use Supabase\Postgrest\PostgrestClient;
 
-$scheme = 'https://';
-$domain = '.supabase.co/';
-$path = 'rest/v1/';
-$opts = [];
+
 $client = new PostgrestClient($reference_id, $api_key);
 $response = $client->rpc('hello_world')->execute();
-//$output = json_decode($response->getBody(), true);
 $output = $response;
 print_r($output);
