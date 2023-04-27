10b6813e9f63f503ec1439a1f8864bed288af51f
Norm <tehnorm@users.noreply.github.com>
2023-04-27 19:17:01 +0000

    Apply fixes from StyleCI

--- examples/call-a-postgres-fucntion/call-a-postgres-function-without-arguments.php
+++ examples/call-a-postgres-fucntion/call-a-postgres-function-without-arguments.php
@@ -3,7 +3,6 @@
 require '../header.php';
 use Supabase\Postgrest\PostgrestClient;
 
-
 $client = new PostgrestClient($reference_id, $api_key);
 $response = $client->rpc('hello_world')->execute();
 $output = $response;
