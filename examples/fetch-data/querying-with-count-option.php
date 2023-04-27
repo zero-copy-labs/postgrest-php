29b9f99770b568b18e973b35c9f27da8226b9acb
Norm <tehnorm@users.noreply.github.com>
2023-04-27 22:34:11 +0000

    Apply fixes from StyleCI

--- examples/fetch-data/querying-with-count-option.php
+++ examples/fetch-data/querying-with-count-option.php
@@ -8,4 +8,4 @@ $client = new PostgrestClient($reference_id, $api_key);
 $response = $client->from('countries')->select('*', ['count'=> 'exact', 'head'=> true])->execute();
 $output = $response;
 print_r($output);
-//understand why there is no result printed 
+//understand why there is no result printed
