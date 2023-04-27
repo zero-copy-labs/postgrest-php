2b1971f7883e188dd31ee7fe7fb8196f7a646f26
Norm <tehnorm@users.noreply.github.com>
2023-04-27 22:53:00 +0000

    Apply fixes from StyleCI

--- examples/upsert-data/bulk-upsert-your-data.php
+++ examples/upsert-data/bulk-upsert-your-data.php
@@ -3,7 +3,6 @@
 require '../header.php';
 use Supabase\Postgrest\PostgrestClient;
 
-
 $client = new PostgrestClient($reference_id, $api_key);
 $response = $client->from('countries')->upsert([['id'=>1, 'name'=> 'Albania'], ['id'=>2, 'name'=> 'Armenia']], $opts)->select()->execute();
 $output = $response;
