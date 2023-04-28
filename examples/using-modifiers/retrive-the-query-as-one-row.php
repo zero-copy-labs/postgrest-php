03752a60e9d761b951f55157f27acb81d5850314
Norm <tehnorm@users.noreply.github.com>
2023-04-28 05:32:08 +0000

    Apply fixes from StyleCI

--- examples/using-modifiers/retrive-the-query-as-one-row.php
+++ examples/using-modifiers/retrive-the-query-as-one-row.php
@@ -3,7 +3,6 @@
 require '../header.php';
 use Supabase\Postgrest\PostgrestClient;
 
-
 $client = new PostgrestClient($reference_id, $api_key);
 $response = $client->from('countries')
 									->select('name')
